09d00b7a46cece9f58f1f9f9a8141903b3efca59
unknown <isaacgabrieljuwon@gmail.com>
2026-01-22 16:32:52 +0100

    fix stkkkkkLLkk

--- resources/views/pages/kyc/create.blade.php
+++ resources/views/pages/kyc/create.blade.php
@@ -179,7 +179,7 @@ new class extends Component
                     <x-ui.input wire:model.live="email" type="email" />
                 </x-ui.field>
 
-                @if ($verificationMode === VerificationMode::Manual->value)
+                @if ($verificationMode === VerificationMode::Manual)
                     <x-ui.field>
                         <x-ui.label>Upload Document (required)</x-ui.label>
                         <x-ui.input 
