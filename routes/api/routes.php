557c1d0d5094b980d9b95e807133400529aa355f
unknown <isaacgabrieljuwon@gmail.com>
2026-04-07 14:21:03 +0100

    djd

--- routes/api/routes.php
+++ routes/api/routes.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1')->group(base_path(
-    path: 'routes/api/v1.php',
-));
+
+
+Route::prefix('v1')->group(function () {
+    require base_path('routes/api/v1.php');
+});
