041d176f22f82f11e3ac46fdddc8a8bf437801b5
unknown <isaacgabrieljuwon@gmail.com>
2026-04-07 13:33:23 +0100

    dasfaek

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -34,7 +34,6 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api/routes.php',
-        apiPrefix: '',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
